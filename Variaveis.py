f7abb2d6ade0dddaa354d09b5baedae5f19ada95
BibiaSF <biasilvafreitas09@gmail.com>
2024-04-23 16:47:55 -0300

    criação variaveis.py

---
+++ Variaveis.py
@@ -0,0 +1,12 @@
+nome="Bibia"
+idade=17
+email='biasilvafreitas09@gmail.com.br'
+telefone='17 988471934'
+nota=7.00
+aprovada=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovada)
